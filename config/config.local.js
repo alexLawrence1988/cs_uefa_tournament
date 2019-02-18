8415d90c38acafab793dcd1860d15df827aa0e8d
Alex Lawrence <alexjlawrence88@gmail.com>
2019-02-18 12:33:18 +0000

    include configs

---
+++ config/config.local.js
@@ -0,0 +1,18 @@
+module.exports = {
+    port: 8500,   
+    domain: 'http://localhost', 
+    databaseConfig: {
+        user: 'admin',
+        password: 'portsmouth1',
+        server: 'thepuntersedge.ctmejohupkg5.eu-west-2.rds.amazonaws.com', // You can use 'localhost\\instance' to connect to named instance
+        database: 'ClubSparkTournament',
+    },
+    SPROCS:{
+        getTournamentTeams: 'dbo.getTournamentDetails',
+        updateTeam: 'dbo.updateTeam',
+        getTournaments: 'dbo.getTournaments'
+    },
+    api: {
+        secretKey: 'clubSparkSecretKey'
+    }
+}
